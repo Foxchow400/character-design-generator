189834665abe33330d854bdf9a9a39275a374866
Foxchow400 <foxchow400@outlook.com>
2022-06-27 17:36:46 -0600

    fixed commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var gender;
+var species;
+
+function $(div) {
+    return document.getElementById(div);
+}
+
+function display(g) {
+    $("output").innerHTML = "<section>" +
+    "Eat a bag of dicks." +
+    "</section>"
+}
+
+function generate() {
+    gender = $("gender").value;
+    function display(gender);
+}
